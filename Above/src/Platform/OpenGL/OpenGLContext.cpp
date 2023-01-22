05c82917ddf75f20fcad0cd05d527638cb902121
Lorenzo HS <lorenzohs98@gmail.com>
2023-01-22 17:53:04 +0100

    Casted some GLAD information to const char* as it's going crazy in some computers. Added information as comments

--- Above/src/Platform/OpenGL/OpenGLContext.cpp
+++ Above/src/Platform/OpenGL/OpenGLContext.cpp
@@ -19,9 +19,16 @@ namespace Above
 		AB_CORE_ASSERT(status, "FAILED TO INITIALIZE GLAD");
 
 		AB_CORE_INFO("- OpenGL info -");
-		AB_CORE_INFO("    Vendor: {0}", glGetString(GL_VENDOR));
-		AB_CORE_INFO("    Renderer: {0}", glGetString(GL_RENDERER));
-		AB_CORE_INFO("    Version {0}", glGetString(GL_VERSION));
+
+		//@Loren
+		//I've been doing a bit of research on this as only some computers need a cast to char*
+		// AFAIK:
+		//Depending on the order of the linking proccess/includes, glGetString can conflict with the logger
+		//as it won't know if GLubyte == const unsigned char*. It will try a static_cast and it will
+		//retrieve an error, as it's not doable as far as it knows at that point.
+		AB_CORE_INFO("    Vendor: {0}", reinterpret_cast<const char*>	(glGetString(GL_VENDOR)));
+		AB_CORE_INFO("    Renderer: {0}", reinterpret_cast<const char*>	(glGetString(GL_RENDERER)));
+		AB_CORE_INFO("    Version {0}", reinterpret_cast<const char*>	(glGetString(GL_VERSION)));
 	}
 
 	void OpenGLContext::SwapBuffers()
