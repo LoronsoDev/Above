750b957451fd8169ea8c95e2f2205a96d6070002
Lorenzo HS <lorenzohs98@gmail.com>
2023-09-08 11:46:57 +0200

    Added transforms to submission (38)

--- Above/src/Above/Renderer/Renderer.h
+++ Above/src/Above/Renderer/Renderer.h
@@ -12,7 +12,7 @@ namespace Above
 		static void BeginScene(OrthographicCamera& camera); //TODO
 		static void EndScene();
 
-		static void Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray);
+		static void Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray, const glm::mat4& transform = glm::mat4(1.0f));
 
 		static inline RendererAPI::API GetAPI() { return RendererAPI::GetAPI(); }
 
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -2,13 +2,16 @@
 
 #include <Above.h>
 
+#include "glm/ext/matrix_transform.hpp"
+
 class ExampleLayer : public Above::Layer
 {
 public:
 	ExampleLayer() 
 		: Layer("Example"),
 		m_Camera(-1.6f, 1.6f, -0.9f, 0.9f),
-		m_CameraPosition(0.f)
+		m_CameraPosition(0.f),
+		m_SquarePosition(0.f)
 	{
 		m_VertexArray.reset(Above::VertexArray::Create());
 
@@ -42,10 +45,10 @@ public:
 
 		float squareVertices[3 * 4] =
 		{
-			-0.75f, -0.75f, 0.0f,
-			 0.75f, -0.75f, 0.0f,
-			 0.75f,  0.75f, 0.0f,
-			-0.75f,  0.75f, 0.0f
+			-.5f, -.5f, 0.0f,
+			 .5f, -.5f, 0.0f,
+			 .5f,  .5f, 0.0f,
+			-.5f,  .5f, 0.0f
 		};
 
 		std::shared_ptr<Above::VertexBuffer> squareVB;
@@ -67,13 +70,14 @@ public:
 			layout(location = 0) in vec3 a_Position;	
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;	
 
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -99,6 +103,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;	
 			out vec4 v_Color;
@@ -107,7 +112,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -148,6 +153,7 @@ public:
 			m_CameraPosition.x += m_CameraMoveSpeed * ts;
 		}
 
+
 		if (Above::Input::IsKeyPressed(AB_KEY_RIGHT))
 		{
 			m_CameraRotation -= m_CameraRotationSpeed * ts;
@@ -158,7 +164,6 @@ public:
 			m_CameraRotation += m_CameraRotationSpeed * ts;
 		}
 
-
 		Above::RenderCommand::SetClearColor({ .1f, .1f, .1f, 1.0f });
 		Above::RenderCommand::Clear();
 
@@ -167,7 +172,18 @@ public:
 
 		Above::Renderer::BeginScene(m_Camera);
 
-		Above::Renderer::Submit(m_BlueShader, m_SquareVA);
+		static glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		for(int y = 0; y < 20; ++y)
+		{
+			for (int x = 0; x < 20; ++x)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				Above::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+			}
+		}
+
 		Above::Renderer::Submit(m_Shader, m_VertexArray);
 
 		Above::Renderer::EndScene();
