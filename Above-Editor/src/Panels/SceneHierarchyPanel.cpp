ac37900facbc02f691c09b4d9465c67584dfc02c
Lorenzo HS <lorenzohs98@gmail.com>
2023-09-16 19:31:25 +0200

    Added ways to add and remove entities and components with the UI

--- Above-Editor/src/Panels/SceneHierarchyPanel.cpp
+++ Above-Editor/src/Panels/SceneHierarchyPanel.cpp
@@ -33,6 +33,15 @@ namespace Above
 			m_SelectionContext = {};
 		}
 
+		if(ImGui::BeginPopupContextWindow(0, ImGuiPopupFlags_NoOpenOverItems | ImGuiPopupFlags_MouseButtonRight))
+		{
+			if(ImGui::MenuItem("Create empty entity"))
+			{
+				m_Context->CreateEntity("New entity");
+			}
+			ImGui::EndPopup();
+		}
+
 		ImGui::End();
 
 		ImGui::Begin("Inspector");
@@ -40,6 +49,28 @@ namespace Above
 			if(m_SelectionContext)
 			{
 				DrawComponents(m_SelectionContext);
+
+				if(ImGui::Button("Add component"))
+				{
+					ImGui::OpenPopup("AddComp");
+				}
+
+				if(ImGui::BeginPopup("AddComp"))
+				{
+					if (ImGui::MenuItem("Camera"))
+					{
+						m_SelectionContext.AddComponent<CameraComponent>();
+						ImGui::CloseCurrentPopup();
+					}
+
+					if (ImGui::MenuItem("Sprite Renderer"))
+					{
+						m_SelectionContext.AddComponent<SpriteRendererComponent>();
+						ImGui::CloseCurrentPopup();
+					}
+					ImGui::EndPopup();
+				}
+
 			}
 		}
 		
@@ -58,11 +89,28 @@ namespace Above
 			m_SelectionContext = entity;
 		}
 
+		bool entityDeleted = false;
+		if (ImGui::BeginPopupContextItem(0, ImGuiPopupFlags_MouseButtonRight))
+		{
+			if (ImGui::MenuItem("Delete entity"))
+			{
+				entityDeleted = true;
+			}
+			ImGui::EndPopup();
+		}
+
 		if(opened)
 		{
 			ImGui::TreePop();
 		}
 		ImGui::Separator();
+
+		if(entityDeleted)
+		{
+			m_Context->DestroyEntity(entity);
+			if (m_SelectionContext == entity)
+				m_SelectionContext = {};
+		}
 	}
 
 	static void DrawVec3Control(const std::string& label, glm::vec3& values, float resetValue = 0.0f, float columnWidth = 100.f)
@@ -155,9 +203,13 @@ namespace Above
 			ImGui::Separator();
 		}
 
+		const ImGuiTreeNodeFlags treeNodeFlags = ImGuiTreeNodeFlags_DefaultOpen | ImGuiTreeNodeFlags_AllowItemOverlap;
+
 		if (entity.HasComponent<TransformComponent>())
 		{
-			if(ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Transform"))
+			bool open = ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), treeNodeFlags, "Transform");
+
+			if(open)
 			{
 				auto& transform = entity.GetComponent<TransformComponent>();
 
@@ -168,13 +220,15 @@ namespace Above
 				DrawVec3Control("Scale", transform.Scale, 1.0f);
 
 				ImGui::TreePop();
+				
 			};
 			ImGui::Separator();
+			
 		}
 
 		if (entity.HasComponent<CameraComponent>())
 		{
-			if (ImGui::TreeNodeEx((void*)typeid(CameraComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Camera"))
+			if (ImGui::TreeNodeEx((void*)typeid(CameraComponent).hash_code(), treeNodeFlags, "Camera"))
 			{
 				auto& cc = entity.GetComponent<CameraComponent>();
 				auto& camera = cc.Camera;
@@ -276,7 +330,26 @@ namespace Above
 
 		if (entity.HasComponent<SpriteRendererComponent>())
 		{
-			if (ImGui::TreeNodeEx((void*)typeid(SpriteRendererComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Sprite Renderer"))
+			ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2{ 4,4 });
+			bool open = ImGui::TreeNodeEx((void*)typeid(SpriteRendererComponent).hash_code(), treeNodeFlags, "Sprite Renderer");
+			ImGui::SameLine(ImGui::GetWindowWidth() - 25.0f);
+			if (ImGui::Button("+", ImVec2{20,20}))
+			{
+				ImGui::OpenPopup("ComponentSettings");
+			}
+			ImGui::PopStyleVar();
+
+			bool removeComponent = false;
+			if (ImGui::BeginPopup("ComponentSettings"))
+			{
+				if (ImGui::MenuItem("Remove Component"))
+				{
+					removeComponent = true;
+				}
+				ImGui::EndPopup();
+			}
+
+			if (open)
 			{
 				auto& sr = entity.GetComponent<SpriteRendererComponent>();
 
@@ -285,6 +358,10 @@ namespace Above
 				ImGui::TreePop();
 			};
 			ImGui::Separator();
+			if (removeComponent)
+			{
+				entity.RemoveComponent<SpriteRendererComponent>();
+			}
 		}
 	}
 }
